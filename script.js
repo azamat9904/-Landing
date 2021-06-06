c74078b0cfd3270db0f13c2bb8e2f987ec90201d
Azamat9904 <azamatnamialy@gmail.com>
2021-06-07 01:45:16 +0600

    range

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ const swiper2 = new Swiper('.swiper-container2', {
   spaceBetween: 240,
   slidesPerView: 1
 });
+
+const rangeInput1 = document.getElementById("range1");
+const rangeInput2 = document.getElementById("range2");
+
+const mouseMoveHandler = (rangeInput, defaultColor) => () => {
+    const x = rangeInput.value;
+    const color = `linear-gradient(90deg,${defaultColor} ${x}%, rgba(111, 111, 111, 0.25) ${x}%)`;
+    rangeInput.style.background = color;
+}
+
+rangeInput1.addEventListener('mousemove', mouseMoveHandler(rangeInput1, '#025EC5'));
+rangeInput2.addEventListener('mousemove', mouseMoveHandler(rangeInput2, '#FFB62A'));
+
